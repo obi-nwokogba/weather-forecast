6cef1d7c80100caa38a61d6c2f204b42fa4abd5c
Obi Nwokogba <83872952+obi-nwokogba@users.noreply.github.com>
2021-06-30 09:40:28 -0400

    Completed deliverable

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import './App.css';
 import React from "react";
 import WeatherForecast from "./components/WeatherForecast";
+import WeatherData from "./weatherData";
 
 export default function App() {
+
+  /*
+  const fiveDayForecast = []
+
+  for (let i=0; i < WeatherData.length; i++) {
+  fiveDayForecast.push(<WeatherForecast img="{WeatherData[i].img}" conditions="${WeatherData.i.conditions}" time="{WeatherData.i.time}" />);
+  } */
+
+  console.log()
   return (
     <div className="App">
       <h1>My WeatherIcons App</h1>
-      <WeatherForecast />
-      <WeatherForecast />
-      <WeatherForecast />
-      <WeatherForecast />
-      <WeatherForecast />
+      <div class="forecastContainer">
+        {WeatherData.map((day) => (
+          <WeatherForecast img={day.img} conditions={day.conditions} time={day.time} />
+        ))}
+
+      </div>
+
+
+
     </div>
   );
 }
--- src/components/WeatherForecast.js
+++ src/components/WeatherForecast.js
@@ -1,11 +1,13 @@
 import React from "react";
 
-export default function WeatherForecast() {
+export default function WeatherForecast(props) {
     return (
         <div className="weather">
-            <img src="" />
+            <img src={props.img} alt="alt"/>
             <p>conditions:</p>
+            <p>{props.conditions}</p>
             <p>time:</p>
+            <p>{props.time}</p>
         </div>
     );
 }
