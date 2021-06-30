2ab97649f517d78b42d09294c81f88d97d4cca9e
Obi Nwokogba <83872952+obi-nwokogba@users.noreply.github.com>
2021-06-29 21:58:39 -0400

    First commit

---
+++ src/components/WeatherForecast.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function WeatherForecast() {
+    return (
+        <div className="weather">
+            <img src="" />
+            <p>conditions:</p>
+            <p>time:</p>
+        </div>
+    );
+}
